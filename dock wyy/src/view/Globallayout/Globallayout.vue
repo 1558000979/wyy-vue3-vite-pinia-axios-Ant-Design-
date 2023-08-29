85562a3cba1db1e5108cf12701b5748c88eeef80
@23 <1558000979@qq.com>
2023-08-29 11:46:38 +0800

    change

--- dock wyy/src/view/Globallayout/Globallayout.vue
+++ dock wyy/src/view/Globallayout/Globallayout.vue
@@ -5,6 +5,7 @@ import HeadTOP from "./components/headTOP.vue";
 </script>
 
 <template>
+  <div>牛马</div>
 <HeadTOP></HeadTOP>
 </template>
 
--- dock wyy/vite.config.js
+++ dock wyy/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),  Pages({
-    dirs: 'src/views',  // 需要生成路由的文件的目录
-    exclude: ['**/components/*.vue']  // 排除在外的目录，将所有 components 目录下的 .vue 文件排除
-  })],
+  plugins: [vue()],
 })
