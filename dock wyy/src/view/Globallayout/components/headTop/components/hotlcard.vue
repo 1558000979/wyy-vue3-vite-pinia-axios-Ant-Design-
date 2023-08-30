a42f1011cdc4bb681cebc38a54f62d5d21a18c11
YunlongDog <yunlongzzj@gmail.com>
2023-08-30 18:12:26 +0800

    fix:bug

--- dock wyy/src/view/Globallayout/components/headTop/components/hotlcard.vue
+++ dock wyy/src/view/Globallayout/components/headTop/components/hotlcard.vue
@@ -1,161 +1,176 @@
 <script setup>
-import {ref, watch,onMounted} from "vue";
-import Hotlrank from "/src/view/Globallayout/components/headTop/components/hotrank.vue";
-import Searchsuggest from "/src/view/Globallayout/components/headTop/components/searchsuggest.vue";
-import {useSearchHistory}from '/src/stores/searchhisstory.js'
-const props = defineProps({
-  value:{
-    type:String
-  },
-  hasmore:{
-    type:Boolean,
-    default:true
-  }
-})
-const propsvalue = ref(props.value)
-const usehisstory = useSearchHistory()
-const  usehisstorylist = ref()
-const  look = ref(true)
-// 监听搜索记录同步更新搜索历史
-watch([usehisstory,propsvalue],([newVal1,newVal2])=>{
-  usehisstorylist.value = newVal1.UsageRecord.slice(-8)
-  console.log(newVal2,'newVal2')
-})
+    import { ref, watch, onMounted } from 'vue';
+    import Hotlrank from '/src/view/Globallayout/components/headTop/components/hotrank.vue';
+    import Searchsuggest from '/src/view/Globallayout/components/headTop/components/searchsuggest.vue';
+    import { useSearchHistory } from '/src/stores/searchhisstory.js';
+    const props = defineProps({
+        value: {
+            type: String,
+        },
+        hasmore: {
+            type: Boolean,
+            default: true,
+        },
+    });
+    const propsvalue = ref(props.value);
+    const usehisstory = useSearchHistory();
+    const usehisstorylist = ref();
+    const look = ref(true);
+    // 监听搜索记录同步更新搜索历史
+    watch([usehisstory, props], ([newVal1, { value }]) => {
+        usehisstorylist.value = newVal1.UsageRecord.slice(-8);
+        console.log(value, 'newVal2');
+    });
 
-const show = ref(false)
-function open(){
-  show.value = true
-}
-function closed(){
-  show.value = false
-}
-// 查看全部
-function lookAll(){
-  usehisstorylist.value =  usehisstory.UsageRecord
-  look.value = false
-}
-// 删除全部的搜索历史
-function delAll(){
-  usehisstory.$reset()
-}
-onMounted(() => {
-  usehisstorylist.value =  usehisstory.UsageRecord.slice(0, 8); // 截取数组的前八个元素
-});
-defineExpose({
-  open,
-  closed,
-});
+    // 监听props的数据
+    watch(props, (newValue) => {
+        console.log(newValue);
+    });
+
+    const show = ref(false);
+    function open() {
+        show.value = true;
+    }
+    function closed() {
+        show.value = false;
+    }
+    // 查看全部
+    function lookAll() {
+        usehisstorylist.value = usehisstory.UsageRecord;
+        look.value = false;
+    }
+    // 删除全部的搜索历史
+    function delAll() {
+        usehisstory.$reset();
+    }
+    onMounted(() => {
+        usehisstorylist.value = usehisstory.UsageRecord.slice(0, 8); // 截取数组的前八个元素
+    });
+    defineExpose({
+        open,
+        closed,
+    });
 </script>
 
 <template>
-<!-- -->
-<div v-if="show"  class="hotlist_container">
-  <div v-if="props.hasmore">
-<!-- 搜索历史-->
-  <div class="top">
-    <div class="top_left">
-      <span>搜索历史</span>
-      <img @click="delAll" src="/src/assets/headTop/del.png" alt="">
+    <!-- -->
+    <div v-if="show" class="hotlist_container">
+        <div v-if="props.hasmore">
+            <!-- 搜索历史-->
+            <div class="top">
+                <div class="top_left">
+                    <span>搜索历史</span>
+                    <img
+                        @click="delAll"
+                        src="/src/assets/headTop/del.png"
+                        alt=""
+                    />
+                </div>
+                <div class="top_right" v-if="look">
+                    <span @click="lookAll">查看全部</span>
+                </div>
+            </div>
+            <div class="history_container">
+                <div
+                    v-for="(item, index) in usehisstorylist"
+                    :key="index"
+                    class="alone"
+                >
+                    {{ item }}
+                </div>
+            </div>
+            <!--热搜榜-->
+            <Hotlrank />
+        </div>
+        <div v-if="!props.hasmore">
+            <Searchsuggest :keyword="props.value" />
+        </div>
     </div>
-    <div class="top_right" v-if="look">
-      <span @click="lookAll">查看全部</span>
-    </div>
-  </div>
-<div class="history_container">
-  <div v-for="(item,index) in usehisstorylist" :key="index" class="alone">{{ item }}</div>
-</div>
-<!--热搜榜-->
-  <Hotlrank/>
-  </div>
-  <div v-if="!props.hasmore">
-    <Searchsuggest :keyword="props.value"/>
-  </div>
-</div>
 </template>
 
 <style scoped lang="less">
-.hotlist_container{
-  position: absolute;
-  top: 133%;
-  left: -62%;
-  width: 21vw;
-  height: 61vh;
+    .hotlist_container {
+        position: absolute;
+        top: 133%;
+        left: -62%;
+        width: 21vw;
+        height: 61vh;
 
-  background-color: #fff;
-  box-shadow: 0 0 18px rgba(0, 0, 0, 1);
-  overflow-y: scroll;
-  .top{
-    display: flex;
-    justify-content: space-between;
-    padding: 1vw;
-    font-size: 14px;
-    .top_left{
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      img{
-        width: 15px;
-        height: 15px;
-        margin-left: .5vw;
-        cursor: pointer;
-      }
+        background-color: #fff;
+        box-shadow: 0 0 18px rgba(0, 0, 0, 1);
+        overflow-y: scroll;
+        .top {
+            display: flex;
+            justify-content: space-between;
+            padding: 1vw;
+            font-size: 14px;
+            .top_left {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                img {
+                    width: 15px;
+                    height: 15px;
+                    margin-left: 0.5vw;
+                    cursor: pointer;
+                }
+            }
+            .top_right {
+                span {
+                    cursor: pointer;
+                }
+            }
+        }
+        .history_container {
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: wrap;
+            margin-top: 20px;
+            margin: 0 1vw;
+            .alone {
+                padding: 7px 20px;
+                border: 1px solid #ececec;
+                border-radius: 20px;
+                margin-bottom: 5px;
+            }
+        }
     }
-    .top_right{
-      span{
-        cursor: pointer;
-      }
+    @media (max-width: 723px) {
+        .hotlist_container {
+            position: absolute;
+            top: 130%;
+            left: -27%;
+            width: 68vw;
+            height: 39vh;
+            padding: 1vw;
+            background-color: #fff;
+            box-shadow: 0 0 18px #000000;
+            overflow-y: scroll;
+        }
     }
-  }
-  .history_container{
-    display: flex;
-    justify-content: space-between;
-   flex-wrap: wrap;
-    margin-top: 20px;
-    margin:0 1vw;
-    .alone{
-      padding: 7px 20px;
-      border: 1px solid #ececec;
-      border-radius: 20px;
-      margin-bottom: 5px;
+    /* WebKit浏览器（Chrome、Safari） */
+    .hotlist_container::-webkit-scrollbar {
+        width: 5px;
     }
-  }
-}
-@media (max-width:723px ) {
-  .hotlist_container{
-    position: absolute;
-    top: 130%;
-    left: -27%;
-    width: 68vw;
-    height: 39vh;
-    padding: 1vw;
-    background-color: #fff;
-    box-shadow: 0 0 18px #000000;
-    overflow-y: scroll;
-  }
-}
-/* WebKit浏览器（Chrome、Safari） */
-.hotlist_container::-webkit-scrollbar {
-  width: 5px;
-}
 
-.hotlist_container::-webkit-scrollbar-track {
-  background: linear-gradient(to bottom, #f6d365, #fda085);
-}
+    .hotlist_container::-webkit-scrollbar-track {
+        background: linear-gradient(to bottom, #f6d365, #fda085);
+    }
 
-.hotlist_container::-webkit-scrollbar-thumb {
-  background-color: #888;
-}
+    .hotlist_container::-webkit-scrollbar-thumb {
+        background-color: #888;
+    }
 
-.hotlist_container::-webkit-scrollbar-thumb:hover {
-  background-color: #555;
-}
+    .hotlist_container::-webkit-scrollbar-thumb:hover {
+        background-color: #555;
+    }
 
-/* Firefox浏览器 */
-html {
-  scrollbar-width: thin;
-}
+    /* Firefox浏览器 */
+    html {
+        scrollbar-width: thin;
+    }
 
-html {
-  scrollbar-color: linear-gradient(to bottom, #f6d365, #fda085) #888;
-}
+    html {
+        scrollbar-color: linear-gradient(to bottom, #f6d365, #fda085) #888;
+    }
 </style>
--- dock wyy/src/view/Globallayout/components/headTop/components/search.vue
+++ dock wyy/src/view/Globallayout/components/headTop/components/search.vue
@@ -1,116 +1,126 @@
-
-
 <template>
-<div class="search_container">
-  <img src="src/assets/headTop/search.png">
-  <input v-model="SearchValue" @keydown.enter="search" @focus="onclickopen" @blur="onclickclosed" type="text" :placeholder=currentHotName  maxlength="18">
-  <Hotlcard :hasmore="hasUse" :value="SearchValue" ref="Rollcall"/>
-</div>
+    <div class="search_container">
+        <img src="src/assets/headTop/search.png" />
+        <input
+            v-model="SearchValue"
+            @keydown.enter="search"
+            @focus="onclickopen"
+            @blur="onclickclosed"
+            type="text"
+            :placeholder="currentHotName"
+            maxlength="18"
+        />
+        <Hotlcard :hasmore="hasUse" :value="SearchValue" ref="Rollcall" />
+    </div>
 </template>
 <script setup>
-import {gethotlist,getsearch,getsearchsuggest}from '/src/request/api/headtop/index.js'
-import Hotlcard from "/src/view/Globallayout/components/headTop/components/hotlcard.vue";
-import {ref, computed, onMounted, onUnmounted, watch} from 'vue';
-import {useSearchHistory}from '/src/stores/searchhisstory.js'
-const usehisstory = useSearchHistory()
-const SearchValue = ref(null);
-let currentIndex = ref(0); // 当前索引
-let intervalId; // 用于存储 setInterval 的 ID
-const hotname = ref('初始值');
-const Rollcall = ref(null)  //子组件ref初始化
-const hasUse = ref()
-watch(SearchValue,(newVal)=>{
-  if (newVal!==null) {
-    hasUse.value = false
-  }else{
-    hasUse.value = true
-  }
-})
-// 获取热搜列表
-function hotlist(){
-  gethotlist().then(res=>{
-    hotname.value = res.data.data.map(item=>item.searchWord)
-  })
-}
-// 搜索
-function search(){
-  // 搜索记录添加进pinia储存
-  usehisstory.UsageRecord.push(SearchValue.value)
-  let value ={
-    keywords:SearchValue.value
-  }
-  getsearch(value).then(res=>{
-    console.log(res,'res')
-  })
-  getsearchsuggest(value).then(res=>{
-    console.log(res,'搜索建议')
-  })
-}
-// 开启与关闭热搜弹窗卡片
-function onclickopen(){
-  Rollcall.value.open()
-}
-function onclickclosed(){
-  Rollcall.value.closed()
-}
-// 动态更换输入框预选值
-function startInterval() {
-  intervalId = setInterval(() => {
-    currentIndex.value = (currentIndex.value + 1) % hotname.value.length;
-  }, 5000); // 每5秒执行一次
-}
-const currentHotName = computed(() => {
-  return hotname.value[currentIndex.value];
-});
+    import {
+        gethotlist,
+        getsearch,
+        getsearchsuggest,
+    } from '/src/request/api/headtop/index.js';
+    import Hotlcard from '/src/view/Globallayout/components/headTop/components/hotlcard.vue';
+    import { ref, computed, onMounted, onUnmounted, watch } from 'vue';
+    import { useSearchHistory } from '/src/stores/searchhisstory.js';
+    const usehisstory = useSearchHistory();
+    const SearchValue = ref(null);
+    let currentIndex = ref(0); // 当前索引
+    let intervalId; // 用于存储 setInterval 的 ID
+    const hotname = ref('初始值');
+    const Rollcall = ref(null); //子组件ref初始化
+    const hasUse = ref();
+    watch(SearchValue, (newVal) => {
+        if (newVal !== null) {
+            hasUse.value = false;
+        } else {
+            hasUse.value = true;
+        }
+    });
+    // 获取热搜列表
+    function hotlist() {
+        gethotlist().then((res) => {
+            hotname.value = res.data.data.map((item) => item.searchWord);
+        });
+    }
+    // 搜索
+    function search() {
+        // 搜索记录添加进pinia储存
+        usehisstory.UsageRecord.push(SearchValue.value);
+        let value = {
+            keywords: SearchValue.value,
+        };
+        getsearch(value).then((res) => {
+            console.log(res, 'res');
+        });
+        getsearchsuggest(value).then((res) => {
+            console.log(res, '搜索建议');
+        });
+    }
+    // 开启与关闭热搜弹窗卡片
+    function onclickopen() {
+        Rollcall.value.open();
+    }
+    function onclickclosed() {
+        Rollcall.value.closed();
+    }
+    // 动态更换输入框预选值
+    function startInterval() {
+        intervalId = setInterval(() => {
+            currentIndex.value =
+                (currentIndex.value + 1) % hotname.value.length;
+        }, 5000); // 每5秒执行一次
+    }
+    const currentHotName = computed(() => {
+        return hotname.value[currentIndex.value];
+    });
 
-// 在组件挂载时开始循环切换值
-onMounted(() => {
-  hotlist();
-  startInterval();
-});
+    // 在组件挂载时开始循环切换值
+    onMounted(() => {
+        hotlist();
+        startInterval();
+    });
 
-// 在组件卸载时清除循环
-onUnmounted(() => {
-  clearInterval(intervalId);
-});
-
+    // 在组件卸载时清除循环
+    onUnmounted(() => {
+        clearInterval(intervalId);
+    });
 </script>
 <style scoped lang="less">
-.search_container{
-  display: flex;
-  align-items: center;
-  font-size: 12px;
-  background-color: #ebebeb;
-  width: 10vw;
-  height: 4vh;
-  border-radius: 20px;
-  padding: 0 20px;
-  margin-left: 3vw;
-  position: relative;
-  img {
-    width: 20px;
-    height: 20px;
-    margin-right: 10px;
-  }
-  input{
-    /* 重置所有边框、内边距和外边距 */
-    border: none;
-    padding: 0;
-    margin: 0;
+    .search_container {
+        display: flex;
+        align-items: center;
+        font-size: 12px;
+        background-color: #ebebeb;
+        width: 10vw;
+        height: 4vh;
+        border-radius: 20px;
+        padding: 0 20px;
+        margin-left: 3vw;
+        position: relative;
+        img {
+            width: 20px;
+            height: 20px;
+            margin-right: 10px;
+        }
+        input {
+            /* 重置所有边框、内边距和外边距 */
+            border: none;
+            padding: 0;
+            margin: 0;
 
-    /* 重置背景色、文本颜色和字体样式 */
-    background-color: transparent;
-    color: inherit;
-    font: inherit;
-  }
-  input:focus {
-    outline: none; /* 取消点击时的外边框效果 */
-  }
-}
-@media (max-width:723px ) {
-  .search_container{
-    width: 45vw;
-    margin-left:18vw;
-  }
-}
+            /* 重置背景色、文本颜色和字体样式 */
+            background-color: transparent;
+            color: inherit;
+            font: inherit;
+        }
+        input:focus {
+            outline: none; /* 取消点击时的外边框效果 */
+        }
+    }
+    @media (max-width: 723px) {
+        .search_container {
+            width: 45vw;
+            margin-left: 18vw;
+        }
+    }
 </style>
