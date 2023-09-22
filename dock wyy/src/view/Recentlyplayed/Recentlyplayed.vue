f9c91a7a31c70b77ff6dfdb9ed1a8c01fc10e429
eastwind <zhanghansan2333@163.com>
2023-09-23 00:00:25 +0800

    添加了最近播放的头部栏

--- dock wyy/src/view/Recentlyplayed/Recentlyplayed.vue
+++ dock wyy/src/view/Recentlyplayed/Recentlyplayed.vue
@@ -1,11 +1,40 @@
 <script setup>
-
 </script>
 
 <template>
-<div>最近播放</div>
+  <div>
+    <div class="headTitle">
+      <div style="float: left; margin-left: 40px">
+        <span style="font-size: 21px; font-weight: bold">最近播放</span>
+        &nbsp;&nbsp;&nbsp;<span
+          style="color: rgb(183, 171, 163); font-size: 15px"
+          >共100首</span
+        >
+      </div>
+      <div style="float: right; margin-right: 30px">
+        <span class="clearList">清空列表</span>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="less">
 
+// 这里去除了lang="less",因为css比较常用,没咋用过less
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.headTitle {
+  width: 100%;
+  height: 85px;
+  margin-top: 25px;
+}
+.clearList {
+  font-size: 18px;
+  color: rgb(80, 168, 216);
+}
+.clearList:hover {
+  color: rgb(11, 88, 176);
+}
 </style>
